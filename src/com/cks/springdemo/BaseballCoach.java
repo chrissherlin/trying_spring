9d013d0550dabb620d3eb7ad224a061eb97a6ae4
chrissherlin <rhymeswithmerlin@gmail.com>
2018-12-19 16:49:27 -0500

    first commit

---
+++ src/com/cks/springdemo/BaseballCoach.java
@@ -0,0 +1,7 @@
+package com.cks.springdemo;
+
+public class BaseballCoach {
+  public String getDailyWorkout() {
+    return "Spend 30 minutes on batting practice";
+  }
+}
---
+++ src/com/cks/springdemo/MyApp.java
@@ -0,0 +1,10 @@
+package com.cks.springdemo;
+
+public class MyApp {
+  public static void main(String[] args) {
+    // create object
+    BaseballCoach theCoach = new BaseballCoach();
+    // use object
+    System.out.println(theCoach.getDailyWorkout());
+  }
+}
